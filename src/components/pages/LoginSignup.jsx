1f6c8f8376d93a8c30616fa73e57fc000c217766
Sagar <sagarpatra641@gmail.com>
2024-09-05 12:07:30 +0530

    login updated

--- src/components/pages/LoginSignup.jsx
+++ src/components/pages/LoginSignup.jsx
@@ -5,37 +5,39 @@ const LoginSignup = () => {
     <>
       <section
         id="login-signup"
-        className=" w-[100%] h-[100%] pt-40 flex items-center justify-center border-none rounded-xl bg-[#fce3fe] py-[auto]"
+        className=" w-[100%] h-[100%] pt-40 flex items-center justify-center border-none rounded-xl bg-[#fce3fe] py-[auto] max-[400px]:pt-[12vh] max-[400px]:h-[100vh] max-[400px]:px-[4vw]"
       >
-        <div className="loginsignup-container w-[580px] h-[auto] bg-white m-auto py-[30px] px-[60px]">
-          <h1 className="my-3 mx-0 text-[32px] font-medium">Sign Up</h1>
+        <div className="loginsignup-container w-[580px] h-[auto] bg-white m-auto py-[30px] px-[60px] max-[400px]:px-[30px] max-[400px]:py-[20px]">
+          <h1 className="my-3 mx-0 text-[32px] font-medium max-[400px]:text-[22px] max-[400px]:underline max-[400px]:py-[0]">
+            Sign Up
+          </h1>
           <div className="loginsignup-fields flex flex-col gap-[30px] mt-[30px]">
             <input
               type="text"
               placeholder="Your name"
               required
-              className="h-[50px] w-[100%] pl-5 border-2 border-[#c9c9c9] outline-none text-[#5c5c5c] text-[18px]"
+              className="h-[50px] w-[100%] pl-5 border-2 border-[#c9c9c9] outline-none text-[#5c5c5c] text-[18px] max-[400px]:h-[35px] max-[400px]:text-[14px]"
             />
             <input
               type="email"
               placeholder="Email address"
               required
-              className="h-[50px] w-[100%] pl-5 border-2 border-[#c9c9c9] outline-none text-[#5c5c5c] text-[18px]"
+              className="h-[50px] w-[100%] pl-5 border-2 border-[#c9c9c9] outline-none text-[#5c5c5c] text-[18px] max-[400px]:h-[35px] max-[400px]:text-[14px]"
             />
             <input
               type="password"
               placeholder="Password"
               required
-              className="h-[50px] w-[100%] pl-5 border-2 border-[#c9c9c9] outline-none text-[#5c5c5c] text-[18px]"
+              className="h-[50px] w-[100%] pl-5 border-2 border-[#c9c9c9] outline-none text-[#5c5c5c] text-[18px] max-[400px]:h-[35px] max-[400px]:text-[14px]"
             />
           </div>
-          <button className="w-[100%] h-[60px] text-white bg-[#ff4141] mt-[30px] border-none rounded-xl text-[24px] font-medium">
+          <button className="w-[100%] h-[60px] text-white bg-[#ff4141] mt-[30px] border-none rounded-xl text-[24px] font-medium max-[400px]:w-[60%] max-[400px]:h-[40px] max-[400px]:text-[18px] max-[400px]:mx-[20%]">
             Continue
           </button>
-          <div className="loginsignup-login mt-[20px] text-[#5c5c5c] text-[18px] font-medium">
+          <div className="loginsignup-login mt-[20px] text-[#5c5c5c] text-[18px] font-medium max-[400px]:text-[13px]">
             Already have an Account?
             <span className="text-[#ff4141] font-semibold"> Login here</span>
-            <div className="loginsignup-agree flex items-center justify-center mt-[25px] gap-2 text-[#5c5c5c] text-[15px] font-medium">
+            <div className="loginsignup-agree flex items-center justify-center mt-[25px] gap-2 text-[#5c5c5c] text-[15px] font-medium max-[400px]:text-[10px]">
               <input type="checkbox" name="" id="" />
               <p>By continuing, I agree to the terms and condition.</p>
             </div>
--- src/components/pages/ShopCategory.jsx
+++ src/components/pages/ShopCategory.jsx
@@ -9,19 +9,21 @@ const ShopCategory = (props) => {
 
   return (
     <>
-      <section id="shop-category">
+      <section id="shop-category" className="max-[400px]:pt-[12vh]">
         {/* shop category banner */}
-        <img src={props.banner} alt="" /* block my-[30px] mx-auto w-[82%] */ />
-        <div className="shopcategory-indexShort flex my-0 mx-[170px] justify-between items-center">
-          <p>
-            <span className="font-semibold">Showing 1-12</span>out of 36
-            products
+        <img src={props.banner} alt="" className="" />
+        <div className="shopcategory-indexShort flex my-0 mx-[170px] justify-between items-center max-[400px]:mx-[10px]">
+          <p className="max-[400px]:flex">
+            <span className="font-semibold max-[400px]:pr-[10px]">
+              Showing 1-12
+            </span>
+            out of 36 products
           </p>
-          <div className="shopcategory-sort py-[10px] px-[20px] rounded-[40px] border-[#888]">
+          <div className="shopcategory-sort py-[10px] px-[20px] rounded-[40px] border-[#888] max-[400px]:flex max-[400px]:items-center">
             Sort by <RiArrowDropDownLine />
           </div>
         </div>
-        <div className="shopcategory-products mx-5 my-[170px] grid grid-cols-4 gap-y-20">
+        <div className="shopcategory-products mx-5 my-[170px] grid grid-cols-4 gap-y-20 max-[400px]:grid-cols-1 max-[400px]:my-[20px] max-[400px]:gap-y-10">
           {all_product.map((item) => {
             if (props.category === item.category) {
               return (
@@ -40,7 +42,7 @@ const ShopCategory = (props) => {
           })}
         </div>
         {/* load more button */}
-        <div className="shopcategory-loadmore flex justify-center items-center my-[150px] mx-auto w-[233px] h-[69px] rounded-[75px] bg-[#ededed] text-[#787878] text-[18px] font-medium">
+        <div className="shopcategory-loadmore flex justify-center items-center my-[150px] mx-auto w-[233px] h-[69px] rounded-[75px] bg-[#ededed] text-[#787878] text-[18px] font-medium max-[400px]:my-[40px] max-[400px]:w-[140px] max-[400px]:h-[40px] max-[400px]:text-[15px]">
           Explore More
         </div>
       </section>
